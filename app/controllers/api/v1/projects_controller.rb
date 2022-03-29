476e735699f54b19e1a1100aa2c873731a384056
DavidKimDY <keayoub@gmail.com>
2022-03-30 08:38:40 +0900

    Add initail controllers

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+        def create
+        end
+
+        def update
+        end
+
+        def read
+        end
+
+        def delete
+        end
+      end
+  end
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+        def create
+        end
+
+        def update
+        end
+
+        def read
+        end
+
+        def delete
+        end
+      end
+  end
+end
