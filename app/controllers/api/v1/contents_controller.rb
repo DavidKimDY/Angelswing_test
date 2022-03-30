9e7fbd2db2cb823621b53f340e566f87bb7900b0
DavidKimDY <keayoub@gmail.com>
2022-03-31 00:38:55 +0900

    Add initial actions in contents controller

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -2,15 +2,23 @@ module Api
   module V1
     class ContentsController < ApplicationController
         def create
+          render json: {status: "Success"}
         end
 
         def update
+          render json: {status: "Success"}
         end
 
         def read
+          render json: {status: "Success"}
+        end
+        
+        def read_all_in_project
+          render json: {status: "Success"}
         end
 
         def delete
+          render json: {status: "Success"}
         end
       end
   end
