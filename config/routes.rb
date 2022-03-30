7f2b4ae54daf22d1006175ffd89f8277ed1c3103
DavidKimDY <keayoub@gmail.com>
2022-03-30 09:48:12 +0900

    Add routes with reference to AW API document

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  namespace 'api' do
+    namespace 'v1' do
+      post '/users/signup'
+      post '/users/signin'
+
+      post '/projects/:id'
+      get '/projects/:id'
+      put '/projects/:id'
+      del '/projects/:id'
+      get '/projects'
+      get '/projects/my_projects'
+
+      post '/projects/:project_id/contents'
+      get '/projects/:project_id/contents'
+      put '/projects/:project_id/contents'
+      get '/projects/:project_id/contents/:id'
+      del '/contents/:id'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
