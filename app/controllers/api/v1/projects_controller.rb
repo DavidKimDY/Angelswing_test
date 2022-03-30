11f6f54f58a4cbb33bf1c03bf2dad51753618e25
DavidKimDY <keayoub@gmail.com>
2022-03-31 00:07:41 +0900

    Edit 4 spaces to 2 spaces indentation

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,64 +1,63 @@
 module Api
   module V1
     class ProjectsController < ApplicationController
-        def create
-          project = Project.new(project_params)
-          if project.save
-            render json: {data: data(project)}, status: :ok
-          else
-            render json: {data: project.errors}, status: :unprocessable_entity 
-          end
+      def create
+        project = Project.new(project_params)
+        if project.save
+          render json: {data: data(project)}, status: :ok
+        else
+          render json: {data: project.errors}, status: :unprocessable_entity 
         end
+      end
 
-        def update
-            project = Project.find(params[:id])
-            if project.update(project_params)
-              render json: {data: data(project)}
-            else
-              render json: {data: project.errors}, status: :unprocessable_entity
-            end
-
+      def update
+        project = Project.find(params[:id])
+        if project.update(project_params)
+          render json: {data: data(project)}
+        else
+          render json: {data: project.errors}, status: :unprocessable_entity
         end
+      end
 
-        def read
-            project = Project.find(params[:id])
-            render json: {data: data(project)}
-        end
+      def read
+        project = Project.find(params[:id])
+        render json: {data: data(project)}
+      end
 
-        def read_all
-          project_all = Project.all
-          render json: {data: data_many(project_all)}
-        end
+      def read_all
+        project_all = Project.all
+        render json: {data: data_many(project_all)}
+      end
 
-        def read_my_projects
-            render json: {status: "success"}
-        end
+      def read_my_projects
+        render json: {status: "success"}
+      end
 
-        def delete
-        end
+      def delete
+      end
 
-        private
-        def data (project)
-          data = {
-            id: project.id,
-            type: "project",
-            attributes: project.attributes.except('id')
-          }
-        return data 
+      private
+      def data (project)
+        data = {
+          id: project.id,
+          type: "project",
+          attributes: project.attributes.except('id')
+        }
+      return data 
+      end
+      
+      def data_many (project_many)
+        data_many = []
+        for project in project_many
+          data_many.append(data(project))
         end
+        return data_many
+      end
         
-        def data_many (project_many)
-          data_many = []
-          for project in project_many
-            data_many.append(data(project))
-          end
-          return data_many
-        end
-          
 
-        def project_params
-          params.require(:project).permit(:title, :project_type, :location, :thumbnail, :user_id, :description)
-        end
+      def project_params
+        params.require(:project).permit(:title, :project_type, :location, :thumbnail, :user_id, :description)
+      end
 
       end
   end
