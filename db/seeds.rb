c0a7c654b830cbeb4c76394ebca893cc7a519344
DavidKimDY <keayoub@gmail.com>
2022-03-30 13:15:18 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Project.destroy_all
+
+kim = User.create(first_name: 'Gildong', last_name: 'Kim', email: "kim@gmail.com")
+project_1 = Project.create(title: 'Seoul Downtown', user_id: kim.id)
+project_2 = Project.create(title: 'Daegu Downtown', user_id: kim.id)
+content_1 = Content.create(title: 'Building', project_id: project_1.id, user_id: project_1.user.id)
+content_1 = Content.create(title: 'Park', project_id: project_1.id, user_id: project_1.user.id)
