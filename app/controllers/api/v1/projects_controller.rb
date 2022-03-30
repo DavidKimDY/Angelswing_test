aaabaa58fee6a407bcaced36747f4e880f8849e4
DavidKimDY <keayoub@gmail.com>
2022-03-30 22:58:23 +0900

    Edit read action to read project by id and add data method to make data format

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -8,9 +8,29 @@ module Api
         end
 
         def read
+            project = Project.find(params[:id])
+            render json: {data: data(project)}
+        end
+
+        def read_all
+            render json: {status: "success"}
+        end
+
+        def read_my_projects
+            render json: {status: "success"}
         end
 
         def delete
+        end
+
+        private
+        def data (project)
+          data = {
+            id: project.id,
+            type: "project",
+            attributes: project.attributes.except('id')
+          }
+        return data 
         end
       end
   end
