7ff61c92d0b9e3fba90d7ee9bc0baa9c7e25b3a1
DavidKimDY <keayoub@gmail.com>
2022-03-31 00:40:07 +0900

    Edit routes to match actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      post '/users/signup'
-      post '/users/signin'
+      post '/users/signup' => 'users#signup'
+      post '/auth/signin' => 'users#signin'
+      
+      post '/projects' => 'projects#create'
+      get '/projects/:id' => 'projects#read'
+      get '/projects' => 'projects#read_all'
+      get '/projects/my_projects' => 'projects#read_my_projects'
+      put '/projects/:id' => 'projects#update'
+      delete '/projects/:id' => 'projects#delete'
 
-      post '/projects/:id'
-      get '/projects/:id'
-      put '/projects/:id'
-      del '/projects/:id'
-      get '/projects'
-      get '/projects/my_projects'
-
-      post '/projects/:project_id/contents'
-      get '/projects/:project_id/contents'
-      put '/projects/:project_id/contents'
-      get '/projects/:project_id/contents/:id'
-      del '/contents/:id'
+      post '/projects/:project_id/contents' => 'contents#create'
+      get '/projects/:project_id/contents/:id' => 'contents#read'
+      get '/projects/:project_id/contents' => 'contents#read_all_in_project'
+      put '/contents/:id' => 'contents#update'
+      delete '/contents/:id' => 'contents#delete'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
