5efa309f174e715e4d8656a2deaf7d08f3d8caff
DavidKimDY <keayoub@gmail.com>
2022-03-31 01:28:15 +0900

    Edit create action to create contents

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -2,7 +2,14 @@ module Api
   module V1
     class ContentsController < ApplicationController
         def create
-          render json: {status: "Success"}
+          project = Project.find(params[:project_id])
+          content = Content.new(content_params)
+          content.update(project_id: project.id, user_id: project.user.id)
+          if content.save
+            render json: {data: data(content)}, status: :ok
+          else
+            render json: {data: content.errors}, status: :unprocessable_entity 
+          end
         end
 
         def update
@@ -20,6 +27,28 @@ module Api
         def delete
           render json: {status: "Success"}
         end
+
+        private
+        def content_params
+          params.require(:content).permit(:title, :body)
+        end
+        
+        def data (content)
+          data = {
+            id: content.id,
+            type: "content",
+            attributes: {
+              "projectId": content.project.id,
+              "projectOwnerName": content.project.user.last_name + content.project.user.first_name,
+              "title": content.title, 
+              "body": content.body,
+              "createdAt": content.created_at,
+              "updatedAt": content.updated_at
+            }
+          }
+          return data
+        end
+
       end
   end
 end
