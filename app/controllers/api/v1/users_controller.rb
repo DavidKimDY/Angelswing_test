ca8c6823c26ade601c02e516f776b076ff0e2402
DavidKimDY <keayoub@gmail.com>
2022-03-31 12:44:52 +0900

    Edit signin action to get signin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,53 @@
 module Api
   module V1
     class UsersController < ApplicationController
-        def signin
-            render json: {status: "success"}
-        end
-    
-        def signup
-            render json: {status: "success"}
+      before_action :authorized, only: [:auto_login]
+
+      def signin
+        @user = User.find_by(email: params[:auth][:email])
+        puts @user
+        if @user && @user.authenticate(params[:auth][:password])
+          render json: {data: data}
+        else
+          render json: {error: "Invalid username or password"}
         end
       end
+    
+      def signup
+        @user = User.create(user_params)
+        if @user.valid?
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {error: "Invalid username or password"}
+        end
+      end
+
+      def auto_login
+        render json: @user
+      end
+
+      private
+
+      def user_params
+        params.permit(:firstName, :lastName, :email, :password, :country)
+      end
+
+      def data
+        data = {
+          id: @user.id,
+          type: "users",
+          attributes: {
+            token: encode_token({user_id: @user.id}),
+            email: @user.email,
+            name: @user.last_name + " " + @user.first_name,
+            country: @user.country,
+            createdAt: @user.created_at,
+            updatedAt: @user.updated_at
+          }
+        }
+        return data
+      end
+    end
   end
 end
