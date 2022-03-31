6add407cc33a5bd6679fdec3762a27b7ee2f1936
DavidKimDY <keayoub@gmail.com>
2022-03-31 23:41:48 +0900

    Add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     attr_accessor :firstName, :lastName
+    validates :first_name, :last_name, :email, :password, presence: true
+    validates :email, uniqueness: true
     has_many :projects
     has_many :contents
     has_secure_password
