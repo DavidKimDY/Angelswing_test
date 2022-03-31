5f452e175902c15c39a3f7e1eccae029c7881568
DavidKimDY <keayoub@gmail.com>
2022-03-31 12:37:56 +0900

    Add password

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 User.destroy_all
 Project.destroy_all
 
-kim = User.create(first_name: 'Gildong', last_name: 'Kim', email: "kim@gmail.com")
+kim = User.create(first_name: 'Gildong', last_name: 'Kim', email: "kim@gmail.com", password: "123123")
 project_1 = Project.create(title: 'Seoul Downtown', user_id: kim.id)
 project_2 = Project.create(title: 'Daegu Downtown', user_id: kim.id)
 content_1 = Content.create(title: 'Building', project_id: project_1.id, user_id: project_1.user.id)
