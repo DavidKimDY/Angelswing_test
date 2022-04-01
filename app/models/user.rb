ece4fa726e9e3cc479e3896ef42411866f9af8af
DavidKimDY <keayoub@gmail.com>
2022-04-01 20:05:26 +0900

    Add validates to check email form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     attr_accessor :firstName, :lastName
     validates :first_name, :last_name, :email, :password, presence: true
-    validates :email, uniqueness: true
+    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     has_many :projects
     has_many :contents
     has_secure_password
