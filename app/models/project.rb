a1b2a4c955813a015e62026dd27c08d58b55d439
DavidKimDY <keayoub@gmail.com>
2022-04-01 10:10:13 +0900

    Add validates to check data present

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
     belongs_to :user
     has_many :contents, dependent: :destroy
+    validates :title, :project_type, :location, :thumbnail, presence: true, on: :create
 end
