63e455d9135478100727a39b3c46cce9191a0e5c
DavidKimDY <keayoub@gmail.com>
2022-04-04 11:19:47 +0900

    Add description into projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ lee = User.create(first_name: 'Gildong', last_name: 'lee', email: "lee@gmail.com
 kim = User.create(first_name: 'David', last_name: 'Kim', email: "kim@gmail.com", password: "123123")
 
 file = File.open(Rails.root.join('img/disney_land.jpeg'))
-project_1 = Project.create(title: 'Seoul Downtown', user_id: kim.id, project_type: "external", location: "Seoul", thumbnail: file)
-project_2 = Project.create(title: 'Daegu Downtown', user_id: lee.id, project_type: "external", location: "Deagu" ,thumbnail: file)
+project_1 = Project.create(title: 'Seoul Downtown', user_id: kim.id, project_type: "external", location: "Seoul", thumbnail: file, description: "best donwtown")
+project_2 = Project.create(title: 'Daegu Downtown', user_id: lee.id, project_type: "external", location: "Deagu" ,thumbnail: file, description: "best donwtown")
 
 Content.create(title: 'Building',body: 'building',  project_id: project_1.id, user_id: project_1.user.id)
 Content.create(title: 'Park', body: 'park',  project_id: project_2.id, user_id: project_2.user.id)
