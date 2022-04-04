8d9d61088811193a2954c12cb25bdc576ac7d23c
DavidKimDY <keayoub@gmail.com>
2022-04-04 11:19:29 +0900

    Edit schema

--- db/migrate/20220329100306_create_projects.rb
+++ db/migrate/20220329100306_create_projects.rb
@@ -1,10 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.integer :user_id
-      t.string :title
-      t.text :description
-      t.integer :type
+      t.integer :user_id , null: false
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :project_type, null: false
       t.string :location
       t.string :thumbnail
 
--- db/migrate/20220329100307_create_contents.rb
+++ db/migrate/20220329100307_create_contents.rb
@@ -1,9 +1,9 @@
 class CreateContents < ActiveRecord::Migration[7.0]
   def change
     create_table :contents do |t|
-      t.integer :user_id
-      t.integer :project_id
-      t.string :title
+      t.integer :user_id, null: false
+      t.integer :project_id, null: false
+      t.string :title, null: false
       t.text :body
 
       t.timestamps
